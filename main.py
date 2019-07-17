b69443a9cf39b821070e2af33acb5dcd342b91d8
stanleyycheung <stanleyycheung@gmail.com>
2019-07-17 23:46:56 +0100

    Finished FFT

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import numpy as np
 import scipy.io.wavfile as wavfile
-from scipy.fftpack import fft
+from scipy import fftpack
 import matplotlib.pyplot as plt
 
 rate, data = wavfile.read("Sounds/6thE.wav")
+plt.rcParams['figure.figsize'] = 16, 8
 
-data = data.T[0]
-print(data)
-plt.plot(data)
-plt.xlabel(r'Time ($\mu s$)')
+
+fs = 44100
+ts = 1/fs
+
+data = data.T[1]
+fft_out = fftpack.fft(data)
+power = np.abs(fft_out)
+sample_freq = fftpack.fftfreq(fft_out.size, d=ts)
+half = int(len(sample_freq)/2)
+
+peak_freq = sample_freq[power.argmax()]
+print(peak_freq)
+
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Power')
+plt.plot(sample_freq[:half], power[:half]*1/len(power))
 plt.grid()
 plt.show()
-
-
-# fft_out = fft(data)
-# plt.plot(data, np.abs(fft_out))
-# plt.show()
